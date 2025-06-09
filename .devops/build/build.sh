629ba4d099a568583481220af43a93121c177b14
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-06-09 13:53:45 +0300

    Refactor Docker setup: remove obsolete Dockerfile, add new Dockerfiles for app and auth server, update build script, and enhance GitHub Actions workflow for improved caching and context management.

--- .devops/build/build.sh
+++ .devops/build/build.sh
@@ -1,2 +1,8 @@
 # Build the Docker image
-docker build -t ledger11-web -f ./Dockerfile ../../src
+docker build -t ledger11-web -f ./Dockerfile.ledger-eleven-app ../../src
+docker build -t ledger11-authserver -f ./Dockerfile.ledger-eleven-auth-server ../../src
+
+# Debug the size of the context
+docker build -t debug-context -f Dockerfile.debug  ../../src
+docker run --rm debug-context
+
