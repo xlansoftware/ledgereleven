c5d7c4ff725e81057c997eaef216cf560defdee6
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-06-09 14:06:28 +0300

    Update build scripts: add tagging and push options for Docker images in workflows

--- .devops/build/build.sh
+++ .devops/build/build.sh
@@ -6,3 +6,6 @@ docker build -t ledger11-authserver -f ./Dockerfile.ledger-eleven-auth-server ..
 docker build -t debug-context -f Dockerfile.debug  ../../src
 docker run --rm debug-context
 
+# Tag
+git tag test-v1.0.0
+git push origin test-v1.0.0
