2373a71d0a0c380aa6744fe5c9ec26ddca8c6a81
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-06-17 06:41:14 +0300

    feat: enhance AuthTokenController with improved logging, validation, and security measures

--- src/ledger11.auth/Program.cs
+++ src/ledger11.auth/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 builder.Services.AddAuthorization();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
