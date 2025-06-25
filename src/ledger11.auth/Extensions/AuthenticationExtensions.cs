d62e0ce6a0111ae4d2724aa1432c9b06bea139c3
Stoyan Krastev (skkdevcraft) <163518570+skkdevcraft@users.noreply.github.com>
2025-06-25 10:23:40 +0300

    chore: refactor the long AddMultiProviderAuthentication method

--- src/ledger11.auth/Extensions/AuthenticationExtensions.cs
+++ src/ledger11.auth/Extensions/AuthenticationExtensions.cs
@@ -27,6 +27,7 @@ public class TwitterAuthSettings : AuthProviderSettings
     public string ConsumerSecret => ClientSecret;
     public bool RetrieveUserDetails { get; set; } = true;
 }
+
 public static class AuthenticationExtensions
 {
     public static AuthenticationBuilder AddMultiProviderAuthentication(
@@ -36,7 +37,7 @@ public static class AuthenticationExtensions
     {
         // Google
         var googleSettings = config.GetSection("Authentication:Google").Get<AuthProviderSettings>();
-        if (googleSettings?.Enable == true && ValidateSettings(googleSettings, "Google"))
+        if (googleSettings?.Enable == true && ValidateSettings(googleSettings, "Google", logger))
         {
             builder.AddGoogle(options =>
             {
@@ -48,7 +49,7 @@ public static class AuthenticationExtensions
 
         // Facebook
         var fbSettings = config.GetSection("Authentication:Facebook").Get<AuthProviderSettings>();
-        if (fbSettings?.Enable == true && ValidateSettings(fbSettings, "Facebook"))
+        if (fbSettings?.Enable == true && ValidateSettings(fbSettings, "Facebook", logger))
         {
             builder.AddFacebook(options =>
             {
@@ -60,7 +61,7 @@ public static class AuthenticationExtensions
 
         // Microsoft
         var microsoftSettings = config.GetSection("Authentication:Microsoft").Get<AuthProviderSettings>();
-        if (microsoftSettings?.Enable == true && ValidateSettings(microsoftSettings, "Microsoft"))
+        if (microsoftSettings?.Enable == true && ValidateSettings(microsoftSettings, "Microsoft", logger))
         {
             builder.AddMicrosoftAccount(options =>
             {
@@ -72,7 +73,7 @@ public static class AuthenticationExtensions
 
         // Twitter (uses extended settings)
         var twitterSettings = config.GetSection("Authentication:Twitter").Get<TwitterAuthSettings>();
-        if (twitterSettings?.Enable == true && ValidateSettings(twitterSettings, "Twitter"))
+        if (twitterSettings?.Enable == true && ValidateSettings(twitterSettings, "Twitter", logger))
         {
             builder.AddTwitter(options =>
             {
@@ -85,115 +86,124 @@ public static class AuthenticationExtensions
 
         // GitHub OAuth
         var githubSettings = config.GetSection("Authentication:GitHub").Get<AuthProviderSettings>();
-        if (githubSettings?.Enable == true && ValidateSettings(githubSettings, "GitHub"))
+        if (githubSettings?.Enable == true && ValidateSettings(githubSettings, "GitHub", logger))
         {
-            builder.AddOAuth("GitHub", options =>
-            {
-                options.ClientId = githubSettings.ClientId;
-                options.ClientSecret = githubSettings.ClientSecret;
-
-                options.SignInScheme = IdentityConstants.ExternalScheme;
-
-                options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
-                options.TokenEndpoint = "https://github.com/login/oauth/access_token";
-                options.UserInformationEndpoint = "https://api.github.com/user";
-
-                options.CallbackPath = new PathString("/signin-github");
-
-                options.Scope.Add("user:email"); // Request email scope
-
-                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
-                options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-                options.ClaimActions.MapJsonKey("urn:github:name", "name");
-                options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-
-                options.SaveTokens = true;
-
-                options.Events = new OAuthEvents
-                {
-                    OnCreatingTicket = async context =>
-                    {
-                        var logger = context.HttpContext.RequestServices
-                            .GetRequiredService<ILoggerFactory>()
-                            .CreateLogger("ledger11.OAuth.GitHub");
-
-                        logger.LogInformation("Fetching GitHub user profile...");
-
-                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-                        request.Headers.UserAgent.ParseAdd("LedgerEleven");
-
-                        var response = await context.Backchannel.SendAsync(
-                            request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                        response.EnsureSuccessStatusCode();
-
-                        using var user = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-                        context.RunClaimActions(user.RootElement);
-
-                        logger.LogInformation("GitHub user profile loaded. Now requesting email...");
-
-                        var emailRequest = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/emails");
-                        emailRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-                        emailRequest.Headers.UserAgent.ParseAdd("LedgerEleven");
-
-                        var emailResponse = await context.Backchannel.SendAsync(
-                            emailRequest, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                        emailResponse.EnsureSuccessStatusCode();
-
-                        var emailsText = await emailResponse.Content.ReadAsStringAsync();
-                        logger.LogDebug("GitHub email JSON: {EmailJson}", emailsText);
-
-                        using var emails = JsonDocument.Parse(emailsText);
-
-                        var email = emails.RootElement
-                            .EnumerateArray()
-                            .FirstOrDefault(e =>
-                                e.GetProperty("primary").GetBoolean() &&
-                                e.GetProperty("verified").GetBoolean())
-                            .GetProperty("email")
-                            .GetString();
-
-                        if (!string.IsNullOrEmpty(email))
-                        {
-                            context.Identity!.AddClaim(new Claim(ClaimTypes.Email, email));
-                            logger.LogInformation("Primary verified email added to claims");
-                        }
-                        else
-                        {
-                            logger.LogWarning("No primary verified email found in GitHub response.");
-                        }
-                    },
-
-                    OnRemoteFailure = context =>
-                    {
-                        var logger = context.HttpContext.RequestServices
-                            .GetRequiredService<ILoggerFactory>()
-                            .CreateLogger("ledger11.OAuth.GitHub");
-                        logger.LogError("GitHub OAuth failure: {Message}", context.Failure?.Message);
-                        return Task.CompletedTask;
-                    }
-                };
-            });
-            logger?.LogInformation("GitHub authentication configured");
+            AddGithubProviderAuthentication(builder, githubSettings, logger);
         }
+
         return builder;
+    }
 
-        bool ValidateSettings(AuthProviderSettings settings, string providerName)
+    private static void AddGithubProviderAuthentication(
+        AuthenticationBuilder builder,
+        AuthProviderSettings settings,
+        ILogger? logger)
+    {
+        builder.AddOAuth("GitHub", "GitHub", options =>
         {
-            if (string.IsNullOrEmpty(settings.ClientId))
-            {
-                logger?.LogWarning($"Missing ClientId for {providerName} authentication");
-                return false;
-            }
+            options.ClientId = settings.ClientId;
+            options.ClientSecret = settings.ClientSecret;
 
-            if (string.IsNullOrEmpty(settings.ClientSecret))
-            {
-                logger?.LogWarning($"Missing ClientSecret for {providerName} authentication");
-                return false;
-            }
+            options.SignInScheme = IdentityConstants.ExternalScheme;
 
-            return true;
+            options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+            options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+            options.UserInformationEndpoint = "https://api.github.com/user";
+
+            options.CallbackPath = new PathString("/signin-github");
+
+            options.Scope.Add("user:email"); // Request email scope
+
+            options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+            options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+            options.ClaimActions.MapJsonKey("urn:github:name", "name");
+            options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+
+            options.SaveTokens = true;
+
+            options.Events = new OAuthEvents
+            {
+                OnCreatingTicket = async context =>
+                {
+                    var logger = context.HttpContext.RequestServices
+                        .GetRequiredService<ILoggerFactory>()
+                        .CreateLogger("ledger11.OAuth.GitHub");
+
+                    logger.LogInformation("Fetching GitHub user profile...");
+
+                    var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                    request.Headers.UserAgent.ParseAdd("LedgerEleven");
+
+                    var response = await context.Backchannel.SendAsync(
+                        request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                    response.EnsureSuccessStatusCode();
+
+                    using var user = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                    context.RunClaimActions(user.RootElement);
+
+                    logger.LogInformation("GitHub user profile loaded. Now requesting email...");
+
+                    var emailRequest = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user/emails");
+                    emailRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                    emailRequest.Headers.UserAgent.ParseAdd("LedgerEleven");
+
+                    var emailResponse = await context.Backchannel.SendAsync(
+                        emailRequest, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                    emailResponse.EnsureSuccessStatusCode();
+
+                    var emailsText = await emailResponse.Content.ReadAsStringAsync();
+                    logger.LogDebug("GitHub email JSON: {EmailJson}", emailsText);
+
+                    using var emails = JsonDocument.Parse(emailsText);
+
+                    var email = emails.RootElement
+                        .EnumerateArray()
+                        .FirstOrDefault(e =>
+                            e.GetProperty("primary").GetBoolean() &&
+                            e.GetProperty("verified").GetBoolean())
+                        .GetProperty("email")
+                        .GetString();
+
+                    if (!string.IsNullOrEmpty(email))
+                    {
+                        context.Identity!.AddClaim(new Claim(ClaimTypes.Email, email));
+                        logger.LogInformation("Primary verified email added to claims");
+                    }
+                    else
+                    {
+                        logger.LogWarning("No primary verified email found in GitHub response.");
+                    }
+                },
+
+                OnRemoteFailure = context =>
+                {
+                    var logger = context.HttpContext.RequestServices
+                        .GetRequiredService<ILoggerFactory>()
+                        .CreateLogger("ledger11.OAuth.GitHub");
+                    logger.LogError("GitHub OAuth failure: {Message}", context.Failure?.Message);
+                    return Task.CompletedTask;
+                }
+            };
+        });
+        logger?.LogInformation("GitHub authentication configured");
+    }
+
+    private static bool ValidateSettings(AuthProviderSettings settings, string providerName, ILogger? logger)
+    {
+        if (string.IsNullOrEmpty(settings.ClientId))
+        {
+            logger?.LogWarning($"Missing ClientId for {providerName} authentication");
+            return false;
         }
+
+        if (string.IsNullOrEmpty(settings.ClientSecret))
+        {
+            logger?.LogWarning($"Missing ClientSecret for {providerName} authentication");
+            return false;
+        }
+
+        return true;
     }
 }
