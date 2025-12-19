dd99f3be8c332bd0d0bf18aadc14c3394466c5ca
Stoyan Krastev <163518570+skkdevcraft@users.noreply.github.com>
2025-12-19 09:00:24 +0200

    Use non-root app container user (USER app) (#47)
    * USER app
    
    * inspect docker compose logs after tests
    
    * using named volume for the data

--- .devops/test/web/test.sh
+++ .devops/test/web/test.sh
@@ -1,1 +1,5 @@
+# docker run --rm mcr.microsoft.com/dotnet/aspnet:9.0 id app
+# docker compose exec ledgerapp touch /data/test
+# docker compose exec ledgerapp ls -ld /data
+# docker compose down -v
 docker-compose run --build --rm test
